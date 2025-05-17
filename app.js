a2b4656bfc077f90730029532c87d5e32fd2a98d
bhmbcio <114215427+bhmbcio@users.noreply.github.com>
2025-05-17 10:45:18 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ if ('serviceWorker' in navigator) { // Проверяем поддержку браузером
     });
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    const qrImage = document.getElementById('qrImage');
+
+    const qrCodes = {
+        btnEmail: 'images/qrcode-email.png',
+        btnTelegram: 'images/qrcode-telegram.png',
+        btnGitHub: 'images/qrcode-github.png'
+    };
+
+    Object.keys(qrCodes).forEach(buttonId => {
+        const btn = document.getElementById(buttonId);
+        btn.addEventListener('click', () => {
+            qrImage.src = qrCodes[buttonId];
+            qrImage.style.display = 'inline-block';
+        });
+    });
+});
+
